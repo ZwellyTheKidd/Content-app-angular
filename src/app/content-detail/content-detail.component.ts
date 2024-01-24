3726da9c66b2ff32962980d57ab30262b94e2e98
ZwellyTheKidd <zwelly.thekidd@outlook.com>
2024-01-24 09:52:31 +0200

    edit page

--- src/app/content-detail/content-detail.component.ts
+++ src/app/content-detail/content-detail.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding, OnDestroy } from '@angular/core';
+import { ThemeService } from '../theme.service';
+import { Subscription } from 'rxjs';
+
 
 @Component({
   selector: 'app-content-detail',
@@ -6,5 +9,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./content-detail.component.css']
 })
 export class ContentDetailComponent {
+  private subscription: Subscription;
+
+  // Local variable to hold the current dark mode state
+  isDarkMode!: boolean;
+
+  constructor(private themeService: ThemeService) {
+    // Subscribe to the theme service
+    this.subscription = this.themeService.darkMode$.subscribe((darkMode) => {
+      // Update the local variable when the theme changes
+      this.isDarkMode = darkMode;
+    });
+  }
+
+  // Bind the class based on the local variable
+  @HostBinding('class.dark') get mode() {
+    return this.isDarkMode;
+  }
+
+  ngOnDestroy() {
+    // Unsubscribe to prevent memory leaks
+    this.subscription.unsubscribe();
+  }
 
 }
